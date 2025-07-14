8df99603bcde3f841a52223babff719d3a0b0aa2
JUHE <13966915864@163.com>
2025-07-15 01:56:07 +0800

    登录页面-静态

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import '@/assets/main.css'
+import 'animate.css'
+// 导入 Element Plus 图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+
 
 import { createApp } from 'vue'
 import App from '@/App.vue'
@@ -10,3 +15,9 @@ const app = createApp(App)
 // 应用路由
 app.use(router)
 app.mount('#app')
+
+
+// 引入图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
